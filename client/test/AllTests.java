7b8660e8037743a01d11e421cc7f97a1b35be1df
Tyler Boone <tboone@microsoft.com>
2018-06-03 23:49:19 -0700

    fixing tests

--- client/test/AllTests.java
+++ client/test/AllTests.java
@@ -3,7 +3,6 @@ package client.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import wallet.test.WalletTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
