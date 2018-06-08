776773ef401bef5dc7de8e365d3d80df6f7af743
Tyler Boone <tboone@microsoft.com>
2018-06-07 18:08:07 -0700

    commenting out cruft

--- shackleton/src/AddressPage.java
+++ shackleton/src/AddressPage.java
@@ -27,7 +27,7 @@ public class AddressPage
   {
 
   }
-
+/*
   long loadConfirmed = 0;
   long valueUnconfirmed = 0;
 
@@ -147,5 +147,5 @@ public class AddressPage
     lst.addAll(bridge_map.values());
     return lst;
 
-  }
+  }*/
 }
