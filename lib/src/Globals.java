17aa3f3ed9af52e2da79e08ec63d1861f21eb302
fireduck64 <fireduck@gmail.com>
2018-06-19 13:11:59 -0700

    1.1.1-dev

--- lib/src/Globals.java
+++ lib/src/Globals.java
@@ -4,7 +4,7 @@ import java.security.Security;
 
 public class Globals
 {
-  public static final String VERSION = "1.1.0-dev";
+  public static final String VERSION = "1.1.1-dev";
 
   public static final int POW_LOOK_PASSES = 6;
 
