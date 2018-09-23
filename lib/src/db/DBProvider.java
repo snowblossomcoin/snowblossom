168171c67428b31ece42697eb94fa3f0f607bd55
fireduck64 <fireduck@gmail.com>
2018-09-22 20:12:21 -0700

    and missed this file

---
+++ lib/src/db/DBProvider.java
@@ -0,0 +1,37 @@
+package snowblossom.lib.db;
+
+import com.google.protobuf.ByteString;
+import duckutil.Config;
+import snowblossom.lib.ChainHash;
+import snowblossom.lib.DaemonThreadFactory;
+import snowblossom.proto.Block;
+import snowblossom.proto.BlockSummary;
+import snowblossom.proto.Transaction;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class DBProvider
+{
+  private static final Logger logger = Logger.getLogger("snowblossom.db");
+
+  protected Config config;
+
+  public DBProvider(Config config)
+  {
+  }
+
+
+  public void close()
+  {
+  }
+
+  protected abstract DBMap openMap(String name) throws Exception;
+  protected abstract DBMapMutationSet openMutationMapSet(String name) throws Exception;
+
+}
