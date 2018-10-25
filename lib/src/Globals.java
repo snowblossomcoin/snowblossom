daba06465edd3c19fefb8c2d9f2e0a607434c8fe
fireduck64 <fireduck@gmail.com>
2018-10-25 10:13:20 -0700

    1.4.1

--- lib/src/Globals.java
+++ lib/src/Globals.java
@@ -4,7 +4,7 @@ import java.security.Security;
 
 public class Globals
 {
-  public static final String VERSION = "1.4.0-dev";
+  public static final String VERSION = "1.4.1";
 
   public static final int POW_LOOK_PASSES = 6;
 
