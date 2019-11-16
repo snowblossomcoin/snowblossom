1eda7b72b78ab585a143c05e13af11b9d3d1e463
fireduck64 <fireduck@gmail.com>
2019-11-15 22:58:10 -0800

    1.6.0 Release

--- lib/src/Globals.java
+++ lib/src/Globals.java
@@ -4,7 +4,7 @@ import java.security.Security;
 
 public class Globals
 {
-  public static final String VERSION = "1.6.0-rc";
+  public static final String VERSION = "1.6.0-release";
 
   public static final int POW_LOOK_PASSES = 6;
 
