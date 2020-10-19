430e81f017c10270ba44157f8e320ca655ab4b50
fireduck64 <fireduck@gmail.com>
2020-10-18 17:26:49 -0700

    Grpc log level default

--- lib/src/LogSetup.java
+++ lib/src/LogSetup.java
@@ -36,7 +36,7 @@ public class LogSetup
     {
       log_props.setProperty(".level", "FINE");
       log_props.setProperty("io.level", "SEVERE");
-      log_props.setProperty("io.grpc", "SEVERE");
+      log_props.setProperty("io.grpc.level", "SEVERE");
     }
 
     
