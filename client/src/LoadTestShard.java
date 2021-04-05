c2ed862edc09909adf66e7c9550ce0204650070a
fireduck64 <fireduck@gmail.com>
2021-04-04 21:21:41 -0700

    shard load test much faster

--- client/src/LoadTestShard.java
+++ client/src/LoadTestShard.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.SplittableRandom;
 import java.util.logging.Logger;
 import java.util.ArrayList;
+import java.util.TreeMap;
 import snowblossom.lib.*;
 import snowblossom.proto.*;
 import snowblossom.util.proto.*;
@@ -50,23 +51,20 @@ public class LoadTestShard
     }
   }
 
-  private void runLoadTestInner()
+  private void trySend(TreeMap<Integer, LinkedList<TransactionBridge>> spendable_map, SplittableRandom rnd )
     throws Exception
   {
-    long min_send =  50000L;
-    long max_send = 500000L;
-    long send_delta = max_send - min_send;
-    SplittableRandom rnd = new SplittableRandom();
-
-    while(true)
-    {
       try(TimeRecordAuto tra_sendone = TimeRecord.openAuto("LoadTestShard.send_one"))
       {
+        long min_send =  50000L;
+        long max_send = 500000L;
+        long send_delta = max_send - min_send;
         int output_count = 1;
-        long fee = 7500;
+        long fee = 12500;
         while (rnd.nextDouble() < 0.5) output_count++;
 
         LinkedList<TransactionOutput> out_list = new LinkedList<>();
+        long total_out = fee;
         for(int i=0; i< output_count; i++)
         {
           long value = min_send + rnd.nextLong(send_delta);
@@ -77,20 +75,41 @@ public class LoadTestShard
             .setValue(value)
             .setTargetShard(dst_shard)
             .build());
+          total_out += value;
         }
 
+        ArrayList<Integer> source_shards = new ArrayList<>();
+        source_shards.addAll(spendable_map.keySet());
+        Collections.shuffle(source_shards);
+        int source_shard = source_shards.get(0);
 
+        long needed_funds = total_out;        
         TransactionFactoryConfig.Builder tx_config = TransactionFactoryConfig.newBuilder();
+        LinkedList<TransactionBridge> source_list = spendable_map.get(source_shard);
+
+        while(needed_funds > 0L)
+        {
+          if (source_list.size() ==0)
+          {  // Ran out on this shard input
+            spendable_map.remove(source_shard);
+            return;
+          }
+          TransactionBridge br = source_list.pop();
+
+          tx_config.addInputs(br.toUTXOEntry());
+          needed_funds -= br.value;
+        }
+        
+
 
         tx_config.setSign(true);
 
         tx_config.addAllOutputs(out_list);
+        tx_config.setInputSpecificList(true);
         tx_config.setChangeRandomFromWallet(true);
-        //tx_config.setInputConfirmedOnly(true);
-        tx_config.setInputConfirmedThenPending(true);
         tx_config.setFeeUseEstimate(true);
         tx_config.setSplitChangeOver(25000000L);
-        tx_config.setChangeShardId( active_shards.get( rnd.nextInt(active_shards.size() ) ) );
+        tx_config.setChangeShardId( active_shards.get( rnd.nextInt(active_shards.size()) ) );
 
         TransactionFactoryResult res = TransactionFactory.createTransaction(tx_config.build(), client.getPurse().getDB(), client);
 
@@ -133,14 +152,45 @@ public class LoadTestShard
           //Thread.sleep(100);
         }
       }
-      finally
-      {
+  }
 
-        time_record.printReport(System.out);
-        time_record.reset();
+  private void runLoadTestInner()
+    throws Exception
+  {
+    try(TimeRecordAuto tra_sendone = TimeRecord.openAuto("LoadTestShard.runLoadTestInner"))
+    {
+      SplittableRandom rnd = new SplittableRandom();
+
+      TreeMap<Integer, LinkedList<TransactionBridge>> spendable_map = new TreeMap<>();
+
+      for(TransactionBridge br : client.getAllSpendable())
+      {
+        if (!br.spent)
+        {
+          if (!spendable_map.containsKey(br.shard_id))
+          {
+            spendable_map.put(br.shard_id, new LinkedList<TransactionBridge>());
+          }
+          spendable_map.get(br.shard_id).add(br);
+        }
+      }
+
+      //Shuffle
+      for(LinkedList<TransactionBridge> lst : spendable_map.values()) Collections.shuffle(lst);
+
+
+      while(spendable_map.size() > 0)
+      {
+        trySend(spendable_map, rnd);
       }
 
     }
+    finally
+    {
+      time_record.printReport(System.out);
+      time_record.reset();
+    }
+    
   }
 
 }
