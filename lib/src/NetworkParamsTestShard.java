d1ab14ff36b92c491eebfe7f22b663298ccea61a
fireduck64 <fireduck@gmail.com>
2021-07-03 22:06:11 -0700

    8

--- lib/src/NetworkParamsTestShard.java
+++ lib/src/NetworkParamsTestShard.java
@@ -85,8 +85,8 @@ public class NetworkParamsTestShard extends NetworkParams
   public int getMinShardLength() { return 10; }
 
   @Override
-  public int getMaxShardId() {return 6; } //allows 4 shards
-  //public int getMaxShardId() {return 14; } //allows 8 shards
+  //public int getMaxShardId() {return 6; } //allows 4 shards
+  public int getMaxShardId() {return 14; } //allows 8 shards
   //public int getMaxShardId() {return 28+2; } //allows 16 shards
   //public int getMaxShardId() {return 62; } // allows 32 shards
   //public int getMaxShardId() {return 126; } //allows 64 shards
