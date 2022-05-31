5223a778ebb037499de860858f1ae6c0e6a1a2d4
fireduck64 <fireduck@gmail.com>
2022-05-31 12:08:27 -0700

    Docker pull rather than --pull

--- example/public-node/snow-update.sh
+++ example/public-node/snow-update.sh
@@ -3,6 +3,11 @@
 node_image=snowblossom/node:latest
 explorer_image=snowblossom/explorer:latest
 
+# Would use --pull always but older version of docker
+# don't know what that is
+docker pull $node_image
+docker pull $explorer_image
+
 docker container stop snowblossom.explorer
 docker container rm snowblossom.explorer
 
@@ -12,9 +17,9 @@ docker container rm snowblossom.node
 docker volume create snownode
 docker volume create snowexplore
 
-docker run -d --pull always --restart always --name snowblossom.node --network host \
+docker run -d --restart always --name snowblossom.node --network host \
   -v snownode:/data -e snowblossom_addr_index=true -e snowblossom_tx_index=true $node_image
 
-docker run -d --pull always --restart always --name snowblossom.explorer --network host \
+docker run -d --restart always --name snowblossom.explorer --network host \
   -v snowexplore:/data $explorer_image
 
