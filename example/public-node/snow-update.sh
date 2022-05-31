28b58e86a946b2a8212d5b27bb9866bd2bfa8709
fireduck64 <fireduck@gmail.com>
2022-05-31 12:05:39 -0700

    Add index options to snow public nodes

--- example/public-node/snow-update.sh
+++ example/public-node/snow-update.sh
@@ -3,9 +3,6 @@
 node_image=snowblossom/node:latest
 explorer_image=snowblossom/explorer:latest
 
-docker pull $node_image
-docker pull $explorer_image
-
 docker container stop snowblossom.explorer
 docker container rm snowblossom.explorer
 
@@ -15,10 +12,9 @@ docker container rm snowblossom.node
 docker volume create snownode
 docker volume create snowexplore
 
-docker run -d --restart always --name snowblossom.node --network host \
-  -v snownode:/data $node_image
+docker run -d --pull always --restart always --name snowblossom.node --network host \
+  -v snownode:/data -e snowblossom_addr_index=true -e snowblossom_tx_index=true $node_image
 
-docker run -d --restart always --name snowblossom.explorer --network host \
+docker run -d --pull always --restart always --name snowblossom.explorer --network host \
   -v snowexplore:/data $explorer_image
-
 
